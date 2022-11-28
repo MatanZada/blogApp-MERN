f77881bc1c5d25905bebcb8a505ace3d77f3d237
MatanZada <matan.zada1@email.com>
2022-11-28 16:10:15 +0200

    add env

--- backend/config/db/dbConnect.js
+++ backend/config/db/dbConnect.js
@@ -2,13 +2,10 @@ const mongoose = require("mongoose");
 
 const dbConnect = async () => {
   try {
-    await mongoose.connect(
-      "mongodb+srv://bloguser:matan@blog-app.fyhdxqh.mongodb.net/?retryWrites=true&w=majority",
-      {
-        useUnifiedTopology: true,
-        useNewUrlParser: true,
-      }
-    );
+    await mongoose.connect(process.env.MONGODB_URL, {
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+    });
     console.log("Db is Connected Successfully");
   } catch (error) {
     console.log(`Error ${error.message}`);
--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,16 @@
 const express = require("express");
+const dotenv = require("dotenv");
+dotenv.config();
 const dbConnect = require("./config/db/dbConnect");
 
 const app = express();
 //DB
 dbConnect();
 
+app.get("/", (req, res) => {
+  res.json({ msg: "API for blog Application..." });
+});
+
 //server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, console.log(`Server is running ${PORT}`));
-console.log(22);
