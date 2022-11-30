5d9a05597075cbb985ffdf6746ad72eabc9ffc66
MatanZada <matan.zada1@email.com>
2022-11-30 18:26:31 +0200

    fetch all users controller

--- backend/route/users/usersRoute.js
+++ backend/route/users/usersRoute.js
@@ -2,10 +2,12 @@ const express = require("express");
 const {
   userRegisterCtrl,
   loginUserCtrl,
+  fetchUserCtrl,
 } = require("../../controller/users/usersCtrl");
 const userRoutes = express.Router();
 
 userRoutes.post("/register", userRegisterCtrl);
 userRoutes.post("/login", loginUserCtrl);
+userRoutes.get("/", fetchUserCtrl);
 
 module.exports = userRoutes;
