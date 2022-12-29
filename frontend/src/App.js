f89a2c860ef8fce1e67384b75a6fa49949d98940
MatanZada <matan.zada1@email.com>
2022-12-29 12:13:26 +0200

    register user form using formik

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import HomePage from "./components/HomePage/HomePage";
+import Register from "./components/Users/Register/Register";
 
 function App() {
   return (
     <React.Fragment>
       <Routes>
         <Route exact path="/" element={<HomePage />} />
+        <Route path="/register" element={<Register />} />
       </Routes>
     </React.Fragment>
   );
