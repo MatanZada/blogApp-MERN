800c104cb559503bac816ed1183cb1e2072ae558
MatanZada <matan.zada1@email.com>
2022-12-29 12:40:10 +0200

    user registration action

---
+++ frontend/src/redux/slices/users/usersSlices.js
@@ -0,0 +1,30 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+
+import axios from "axios";
+
+//register action
+
+export const registerUserAction = createAsyncThunk(
+  "users/register",
+  async (user, { rejectWithValue, getState, dispatch }) => {
+    try {
+      //http call
+      const config = {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      };
+      const { data } = await axios.post(
+        "http://localhost:8080/api/users/register",
+        user,
+        config
+      );
+      return data;
+    } catch (error) {
+      if (!error?.response) {
+        throw error;
+      }
+      return rejectWithValue(error?.response?.data);
+    }
+  }
+);
