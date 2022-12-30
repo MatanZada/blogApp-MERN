a507b3e20c79fde19ef3f330365a024c8d78e641
Matan Zada <91349313+MatanZada@users.noreply.github.com>
2022-12-30 16:27:46 +0200

    user login form component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import HomePage from "./components/HomePage/HomePage";
+import Login from "./components/Users/Login/Login";
 import Register from "./components/Users/Register/Register";
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route exact path="/" element={<HomePage />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </React.Fragment>
   );
