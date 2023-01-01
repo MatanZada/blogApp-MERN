48f8a63d508dedf415a0a7d852092b8959bb128b
Matan Zada <91349313+MatanZada@users.noreply.github.com>
2023-01-01 15:12:21 +0200

    Navbar - react router dom

--- frontend/src/components/Navigation/Navbar.jsx
+++ frontend/src/components/Navigation/Navbar.jsx
@@ -1,10 +1,13 @@
 import React from "react";
+import AdminNavbar from "./Admin/AdminNavbar";
+import PrivateNavbar from "./Private/PrivateNavbar";
+import PublicNavbar from "./Public/PublicNavbar";
 
 const Navbar = () => {
   return (
-    <div>
-      <div>Navbar</div>
-    </div>
+    <>
+      <AdminNavbar />
+    </>
   );
 };
 
