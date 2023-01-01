45942b1286188b3c2b79eff7c7e7934853b40d28
Matan Zada <91349313+MatanZada@users.noreply.github.com>
2023-01-01 15:52:45 +0200

    Logout User Part 1

--- frontend/src/components/Navigation/Private/PrivateNavbar.jsx
+++ frontend/src/components/Navigation/Private/PrivateNavbar.jsx
@@ -10,6 +10,7 @@ import {
 } from "@heroicons/react/24/outline";
 import { PlusIcon, LogoutIcon } from "@heroicons/react/24/solid";
 import { useDispatch } from "react-redux";
+import { logoutUserAction } from "../../../redux/slices/users/usersSlices";
 
 const navigation = [
   { name: "Home", href: "/", current: true },
@@ -110,6 +111,7 @@ const PrivateNavbar = ({ isLogin }) => {
                   </Link>
 
                   <button
+                    onClick={() => dispatch(logoutUserAction())}
                     type="button"
                     className="relative inline-flex items-center px-4 py-2 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-red-500 hover:bg-indigo-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-indigo-500"
                   >
