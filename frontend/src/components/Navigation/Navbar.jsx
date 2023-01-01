0984d2abc2acc9a899c344381300aa96fdbe1f07
Matan Zada <91349313+MatanZada@users.noreply.github.com>
2023-01-01 15:29:40 +0200

    display navbar base on login user

--- frontend/src/components/Navigation/Navbar.jsx
+++ frontend/src/components/Navigation/Navbar.jsx
@@ -1,12 +1,26 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import AdminNavbar from "./Admin/AdminNavbar";
 import PrivateNavbar from "./Private/PrivateNavbar";
 import PublicNavbar from "./Public/PublicNavbar";
 
 const Navbar = () => {
+  //get user from store
+  const state = useSelector((state) => state.users);
+  // console.log(state);
+  const { userAuth } = state;
+  // console.log(userAuth);
+  const isAdmin = userAuth?.isAdmin;
+  // console.log(isAdmin);
   return (
     <>
-      <AdminNavbar />
+      {!userAuth ? (
+        <PublicNavbar />
+      ) : userAuth ? (
+        <PrivateNavbar />
+      ) : (
+        isAdmin && <AdminNavbar />
+      )}
     </>
   );
 };
