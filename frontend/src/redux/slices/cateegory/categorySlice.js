aaa765445e0a8ad3a367184612e4a774e38956ff
Matan Zada <91349313+MatanZada@users.noreply.github.com>
2023-01-02 15:07:26 +0200

    delete and update category action

--- frontend/src/redux/slices/cateegory/categorySlice.js
+++ frontend/src/redux/slices/cateegory/categorySlice.js
@@ -2,6 +2,10 @@ import { createAsyncThunk, createSlice, createAction } from "@reduxjs/toolkit";
 import axios from "axios";
 import baseUrl from "../../../utils/baseURL";
 
+//action to redirect
+const resetEditAction = createAction("category/reset");
+const resetDeleteAction = createAction("category/delete-reset");
+
 //action
 export const createCategoryAction = createAsyncThunk(
   "category/create",
@@ -35,7 +39,7 @@ export const createCategoryAction = createAsyncThunk(
   }
 );
 
-//action
+//fetch all action
 export const fetchCategoriesAction = createAsyncThunk(
   "category/fetch",
   async (category, { rejectWithValue, getState, dispatch }) => {
@@ -52,6 +56,67 @@ export const fetchCategoriesAction = createAsyncThunk(
     //http call
     try {
       const { data } = await axios.get(`${baseUrl}/api/category`, config);
+      return data;
+    } catch (error) {
+      if (!error?.response) {
+        throw error;
+      }
+      return rejectWithValue(error?.response?.data);
+    }
+  }
+);
+
+//Update
+export const updateCategoriesAction = createAsyncThunk(
+  "category/update",
+  async (category, { rejectWithValue, getState, dispatch }) => {
+    //get user token
+    const user = getState()?.users;
+    const { userAuth } = user;
+    const config = {
+      headers: {
+        Authorization: `Bearer ${userAuth?.token}`,
+      },
+    };
+    //http call
+    try {
+      const { data } = await axios.put(
+        `${baseUrl}/api/category/${category?.id}`,
+        { title: category?.title },
+        config
+      );
+      //dispatch ation to reset the updated data
+      dispatch(resetEditAction());
+      return data;
+    } catch (error) {
+      if (!error?.response) {
+        throw error;
+      }
+      return rejectWithValue(error?.response?.data);
+    }
+  }
+);
+
+//delete
+export const deleteCategoriesAction = createAsyncThunk(
+  "category/delete",
+  async (id, { rejectWithValue, getState, dispatch }) => {
+    //get user token
+    const user = getState()?.users;
+    const { userAuth } = user;
+    const config = {
+      headers: {
+        Authorization: `Bearer ${userAuth?.token}`,
+      },
+    };
+    //http call
+    try {
+      const { data } = await axios.delete(
+        `${baseUrl}/api/category/${id}`,
+        config
+      );
+      //dispatch action
+      dispatch(resetDeleteAction());
       return data;
     } catch (error) {
       if (!error?.response) {
@@ -94,6 +159,46 @@ const categorySlices = createSlice({
       state.serverErr = undefined;
     });
     builder.addCase(fetchCategoriesAction.rejected, (state, action) => {
+      state.loading = false;
+      state.appErr = action?.payload?.message;
+      state.serverErr = action?.error?.message;
+    });
+    //update
+    builder.addCase(updateCategoriesAction.pending, (state, action) => {
+      state.loading = true;
+    });
+    //Dispatch action
+    builder.addCase(resetEditAction, (state, action) => {
+      state.isEdited = true;
+    });
+    builder.addCase(updateCategoriesAction.fulfilled, (state, action) => {
+      state.updateCategory = action?.payload;
+      state.isEdited = false;
+      state.loading = false;
+      state.appErr = undefined;
+      state.serverErr = undefined;
+    });
+    builder.addCase(updateCategoriesAction.rejected, (state, action) => {
+      state.loading = false;
+      state.appErr = action?.payload?.message;
+      state.serverErr = action?.error?.message;
+    });
+    //delete
+    builder.addCase(deleteCategoriesAction.pending, (state, action) => {
+      state.loading = true;
+    });
+    //dispatch for redirect
+    builder.addCase(resetDeleteAction, (state, action) => {
+      state.isDeleted = true;
+    });
+    builder.addCase(deleteCategoriesAction.fulfilled, (state, action) => {
+      state.deletedCategory = action?.payload;
+      state.isDeleted = false;
+      state.loading = false;
+      state.appErr = undefined;
+      state.serverErr = undefined;
+    });
+    builder.addCase(deleteCategoriesAction.rejected, (state, action) => {
       state.loading = false;
       state.appErr = action?.payload?.message;
       state.serverErr = action?.error?.message;
