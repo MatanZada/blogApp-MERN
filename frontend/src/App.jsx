9cb06b41ee8be43536f052f627955f58c36294ac
Matan Zada <91349313+MatanZada@users.noreply.github.com>
2023-01-04 18:18:33 +0200

    Adding a conditional sentence if a user is logged in will stay on the page, and if not drop him to login

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import AddNewCategory from "./components/Categories/AddNewCategory";
 import CategoryList from "./components/Categories/CategoryList";
 import UpdateCategory from "./components/Categories/UpdateCategory";
@@ -7,8 +7,13 @@ import HomePage from "./components/HomePage/HomePage";
 import Navbar from "./components/Navigation/Navbar";
 import Login from "./components/Users/Login/Login";
 import Register from "./components/Users/Register/Register";
+import { useSelector } from "react-redux";
 
-function App() {
+function App({ ...rest }) {
+  //check if user is login
+  const user = useSelector((state) => state?.users);
+  const { userAuth } = user;
+
   return (
     <React.Fragment>
       <Navbar />
@@ -16,9 +21,27 @@ function App() {
         <Route exact path="/" element={<HomePage />} />
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/add-category" element={<AddNewCategory />} />
-        <Route path="/category-list" element={<CategoryList />} />
-        <Route path="/update-category/:id" element={<UpdateCategory />} />
+        <Route
+          path="/add-category"
+          {...rest}
+          element={
+            userAuth ? <AddNewCategory {...rest} /> : <Navigate to="/login" />
+          }
+        />
+        <Route
+          path="/category-list"
+          {...rest}
+          element={
+            userAuth ? <CategoryList {...rest} /> : <Navigate to="/login" />
+          }
+        />
+        <Route
+          path="/update-category/:id"
+          {...rest}
+          element={
+            userAuth ? <UpdateCategory {...rest} /> : <Navigate to="/login" />
+          }
+        />
       </Routes>
     </React.Fragment>
   );
