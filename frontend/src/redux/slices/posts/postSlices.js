ee5691b93c11a46465b11c6f06b82f14c6a6336b
Matan Zada <91349313+MatanZada@users.noreply.github.com>
2023-01-05 19:18:13 +0200

    create post action and reducer

---
+++ frontend/src/redux/slices/posts/postSlices.js
@@ -0,0 +1,55 @@
+import { createAsyncThunk, createSlice, createAction } from "@reduxjs/toolkit";
+import axios from "axios";
+import baseUrl from "../../../utils/baseURL";
+
+//create Post action
+export const createpostAction = createAsyncThunk(
+  "post/created",
+  async (post, { rejectWithValue, getState, dispatch }) => {
+    // console.log(post);
+    //get user token
+    const user = getState()?.users;
+    const { userAuth } = user;
+    const config = {
+      headers: {
+        Authorization: `Bearer ${userAuth?.token}`,
+      },
+    };
+    try {
+      //http call
+      const { data } = await axios.post(`${baseUrl}/api/posts`, post, config);
+      return data;
+    } catch (error) {
+      if (!error?.response) throw error;
+      return rejectWithValue(error?.response?.data);
+    }
+  }
+);
+
+//slice
+
+const postSlice = createSlice({
+  name: "post",
+  initialState: {
+    post: 20,
+  },
+  extraReducers: (builder) => {
+    builder.addCase(createpostAction.pending, (state, action) => {
+      state.loading = true;
+    });
+    builder.addCase(createpostAction.fulfilled, (state, action) => {
+      state.postCreated = action?.payload;
+      state.loading = false;
+      state.isCreated = false;
+      state.appErr = undefined;
+      state.serverErr = undefined;
+    });
+    builder.addCase(createpostAction.rejected, (state, action) => {
+      state.loading = false;
+      state.appErr = action?.payload?.message;
+      state.serverErr = action?.error?.message;
+    });
+  },
+});
+
+export default postSlice.reducer;
