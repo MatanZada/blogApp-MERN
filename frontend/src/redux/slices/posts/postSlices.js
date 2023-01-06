1ccc48cde17f78534fb1c7a5b8f2fcea4746d9af
Matan Zada <91349313+MatanZada@users.noreply.github.com>
2023-01-06 23:16:57 +0200

    posts like action creator

--- frontend/src/redux/slices/posts/postSlices.js
+++ frontend/src/redux/slices/posts/postSlices.js
@@ -60,6 +60,33 @@ export const fetchPostsAction = createAsyncThunk(
   }
 );
 
+//add likes to post
+export const toggleAddLikesToPost = createAsyncThunk(
+  "post/like",
+  async (postId, { rejectWithValue, getState, dispatch }) => {
+    //get user token
+    const user = getState()?.users;
+    const { userAuth } = user;
+    const config = {
+      headers: {
+        Authorization: `Bearer ${userAuth?.token}`,
+      },
+    };
+    try {
+      const { data } = await axios.put(
+        `http://localhost:8080/api/posts/likes`,
+        { postId },
+        config
+      );
+
+      return data;
+    } catch (error) {
+      if (!error?.response) throw error;
+      return rejectWithValue(error?.response?.data);
+    }
+  }
+);
+
 //slice
 
 const postSlice = createSlice({
@@ -96,6 +123,21 @@ const postSlice = createSlice({
       state.serverErr = undefined;
     });
     builder.addCase(fetchPostsAction.rejected, (state, action) => {
+      state.loading = false;
+      state.appErr = action?.payload?.message;
+      state.serverErr = action?.error?.message;
+    });
+    //Likes
+    builder.addCase(toggleAddLikesToPost.pending, (state, action) => {
+      state.loading = true;
+    });
+    builder.addCase(toggleAddLikesToPost.fulfilled, (state, action) => {
+      state.likes = action?.payload;
+      state.loading = false;
+      state.appErr = undefined;
+      state.serverErr = undefined;
+    });
+    builder.addCase(toggleAddLikesToPost.rejected, (state, action) => {
       state.loading = false;
       state.appErr = action?.payload?.message;
       state.serverErr = action?.error?.message;
