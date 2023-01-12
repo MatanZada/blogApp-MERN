693cdc954f08a5417dc2df801c46172da93ecd36
Matan Zada <91349313+MatanZada@users.noreply.github.com>
2023-01-12 17:29:46 +0200

    remove btn email send

--- frontend/src/components/Users/Profile/Profile.jsx
+++ frontend/src/components/Users/Profile/Profile.jsx
@@ -241,30 +241,6 @@ export default function Profile() {
                                   </Link>
                                 )}
                               </>
-                              {/* Send Mail */}
-                              <button
-                                // onClick={sendMailNavigate}
-                                className="inline-flex justify-center bg-indigo-900 px-4 py-2 border border-yellow-700 shadow-sm text-sm font-medium rounded-md text-gray-700  hover:bg-yellow-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-pink-500"
-                              >
-                                <svg
-                                  xmlns="http://www.w3.org/2000/svg"
-                                  fill="none"
-                                  viewBox="0 0 24 24"
-                                  strokeWidth={1.5}
-                                  stroke="currentColor"
-                                  className="-ml-1 mr-2 h-5 w-5 text-gray-200"
-                                >
-                                  <path
-                                    strokeLinecap="round"
-                                    strokeLinejoin="round"
-                                    d="M21.75 6.75v10.5a2.25 2.25 0 01-2.25 2.25h-15a2.25 2.25 0 01-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0019.5 4.5h-15a2.25 2.25 0 00-2.25 2.25m19.5 0v.243a2.25 2.25 0 01-1.07 1.916l-7.5 4.615a2.25 2.25 0 01-2.36 0L3.32 8.91a2.25 2.25 0 01-1.07-1.916V6.75"
-                                  />
-                                </svg>
-
-                                <span className="text-base mr-2  text-bold text-yellow-500">
-                                  Send Message
-                                </span>
-                              </button>
                             </div>
                           </div>
                         </div>
