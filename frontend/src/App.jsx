cdfa897edb205317544dddcf56770058eef93b9b
Matan Zada <91349313+MatanZada@users.noreply.github.com>
2023-01-14 22:00:30 +0200

    users list component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -16,6 +16,7 @@ import UpdateComment from "./components/Comments/UpdateComment";
 import Profile from "./components/Users/Profile/Profile";
 import UploadProfilePhoto from "./components/Users/Profile/UploadProfilePhoto";
 import UpdateProfileForm from "./components/Users/Profile/UpdateProfileForm";
+import UsersList from "./components/Users/UsersList/UsersList";
 
 function App({ ...rest }) {
   //check if user is login
@@ -39,6 +40,14 @@ function App({ ...rest }) {
           {...rest}
           element={
             userAuth ? <UpdateComment {...rest} /> : <Navigate to="/login" />
+          }
+        />
+
+        <Route
+          path="/users"
+          {...rest}
+          element={
+            userAuth ? <UsersList {...rest} /> : <Navigate to="/login" />
           }
         />
 
--- frontend/src/components/Users/Profile/Profile.jsx
+++ frontend/src/components/Users/Profile/Profile.jsx
@@ -30,7 +30,7 @@ export default function Profile() {
   useEffect(() => {
     dispatch(userProfileAction(id));
   }, [id, dispatch, followed, unFollowed]);
-  //   console.log(profile);
+  // console.log(profile?.viewedBy);
 
   //isLogin
   const isLoginUser = userAuth?._id === profile?._id;
@@ -171,7 +171,6 @@ export default function Profile() {
                                           d="M15.182 16.318A4.486 4.486 0 0012.016 15a4.486 4.486 0 00-3.198 1.318M21 12a9 9 0 11-18 0 9 9 0 0118 0zM9.75 9.75c0 .414-.168.75-.375.75S9 10.164 9 9.75 9.168 9 9.375 9s.375.336.375.75zm-.375 0h.008v.015h-.008V9.75zm5.625 0c0 .414-.168.75-.375.75s-.375-.336-.375-.75.168-.75.375-.75.375.336.375.75zm-.375 0h.008v.015h-.008V9.75z"
                                         />
                                       </svg>
-
                                       <span>Unfollow</span>
                                     </button>
                                   ) : (
