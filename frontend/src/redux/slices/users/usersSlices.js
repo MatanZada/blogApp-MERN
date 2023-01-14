9b0d7487da4185f5148bffb30e35fa6a783a20ab
Matan Zada <91349313+MatanZada@users.noreply.github.com>
2023-01-14 22:00:46 +0200

    fetch all users actions

--- frontend/src/redux/slices/users/usersSlices.js
+++ frontend/src/redux/slices/users/usersSlices.js
@@ -248,6 +248,28 @@ export const unfollowUserAction = createAsyncThunk(
   }
 );
 
+//fetch all users
+export const fetchUsersAction = createAsyncThunk(
+  "user/list",
+  async (id, { rejectWithValue, getState, dispatch }) => {
+    //get user token
+    const user = getState()?.users;
+    const { userAuth } = user;
+    const config = {
+      headers: {
+        Authorization: `Bearer ${userAuth?.token}`,
+      },
+    };
+    try {
+      const { data } = await axios.get(`${baseUrl}/api/users`, config);
+      return data;
+    } catch (error) {
+      if (!error?.response) throw error;
+      return rejectWithValue(error?.response?.data);
+    }
+  }
+);
+
 //slices
 
 const usersSlices = createSlice({
@@ -417,6 +439,24 @@ const usersSlices = createSlice({
       state.unfollowLoading = false;
       state.unFollowedAppErr = action?.payload?.message;
       state.unfollowServerErr = action?.error?.message;
+    });
+    //All Users
+    builder.addCase(fetchUsersAction.pending, (state, action) => {
+      state.loading = true;
+      state.appErr = undefined;
+      state.serverErr = undefined;
+    });
+    builder.addCase(fetchUsersAction.fulfilled, (state, action) => {
+      state.loading = false;
+      state.usersList = action?.payload;
+      state.appErr = undefined;
+      state.serverErr = undefined;
+    });
+    builder.addCase(fetchUsersAction.rejected, (state, action) => {
+      console.log(action.payload);
+      state.loading = false;
+      state.appErr = action?.payload?.message;
+      state.serverErr = action?.error?.message;
     });
   },
 });
