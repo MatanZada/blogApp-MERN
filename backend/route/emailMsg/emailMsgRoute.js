8455fc4b68179a3271bcf0e5ea0091c58ff12adc
Matan Zada <91349313+MatanZada@users.noreply.github.com>
2023-01-15 21:30:07 +0200

    add comment

--- backend/route/emailMsg/emailMsgRoute.js
+++ backend/route/emailMsg/emailMsgRoute.js
@@ -2,7 +2,7 @@ const express = require("express");
 const { sendEmailMsgCtrl } = require("../../controller/emailMsg/emailMsgCtrl");
 const authMiddleware = require("../../middlewares/auth/authMiddleware");
 const emailMsgRoute = express.Router();
-
+//route
 emailMsgRoute.post("/", authMiddleware, sendEmailMsgCtrl);
 
 module.exports = emailMsgRoute;
